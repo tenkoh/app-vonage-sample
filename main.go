016fb1855fd42b94350f8ee8f751633112f7f635
tenkoh <tenkoh.go@gmail.com>
2022-11-14 11:44:14 +0900

    mock done

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io"
+	"log"
 	"net/http"
 	"os"
 
@@ -12,69 +15,135 @@ import (
 )
 
 const (
-	sessionName = "vonage-go-sample"
+	sessionName   = "vonage-go-sample"
+	sessionMaxAge = 120
 )
 
-// PROCESS.
-// create session manager
-// create session
-// start verify process
-// save requestID in the session
-// check
-//
-// VIEW.
-// home
-//   if session != nil -> show "VERIFIED USER"
-//   else -> "NOT VERIFIED USER"
-// send
-// verify
+// implatement echo.Template interface
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data any, e echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+var t = &Template{
+	templates: template.Must(template.ParseGlob("views/*.tmpl")),
+}
 
 func home(c echo.Context) error {
 	sess, err := session.Get(sessionName, c)
 	if err != nil {
 		return c.String(http.StatusInternalServerError, fmt.Sprintf("fail to call session: %s", err))
 	}
-	v, exist := sess.Values["verifiedNumber"]
+	v, exist := sess.Values["verified"]
 	if !exist {
-		return c.String(http.StatusOK, "New Comer")
+		v = false
 	}
-	if v == "" {
-		return c.String(http.StatusOK, "New Comer")
-	}
-	return c.String(http.StatusOK, fmt.Sprintf("Verified person: %s", v))
+	return c.Render(http.StatusOK, "home", v)
 }
 
-func send(c echo.Context) error {
-	return nil
-}
-
+// verify handler starts verify process using vonage api.
+// tel number must be passed via form.
 func verify(c echo.Context) error {
+	tel := c.FormValue("tel")
+	log.Printf("requested telephone number: %s\n", tel)
+	// validation
+	if !isValidNumber(tel) {
+		return c.String(http.StatusBadRequest, "invalid telephone number")
+	}
+	// start verify process using vonage api
+	reqID, err := requestVerify(c)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	// save requestID into session
 	sess, err := session.Get(sessionName, c)
 	if err != nil {
 		return c.String(http.StatusInternalServerError, fmt.Sprintf("fail to call session: %s", err))
 	}
 	sess.Options = &sessions.Options{
 		Path:     "/",
-		MaxAge:   60,
+		MaxAge:   sessionMaxAge,
 		HttpOnly: true,
 		Secure:   true,
 	}
-	sess.Values["verifiedNumber"] = "0120100100"
+	sess.Values["requestID"] = reqID
 	sess.Save(c.Request(), c.Response())
-	return c.NoContent(http.StatusOK)
+
+	// show form to send pin code
+	return c.Render(http.StatusOK, "pinform", nil)
+}
+
+func check(c echo.Context) error {
+	pin := c.FormValue("pin")
+	if !isValidPin(pin) {
+		return c.String(http.StatusBadRequest, pin)
+	}
+	verified, err := requestCheck(c)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	if !verified {
+		return c.HTML(
+			http.StatusForbidden,
+			`<html><p>verification failed</p><a href="/">go to home</a></html>`,
+		)
+	}
+
+	sess, err := session.Get(sessionName, c)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("fail to call session: %s", err))
+	}
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   sessionMaxAge,
+		HttpOnly: true,
+		Secure:   true,
+	}
+	sess.Values["verified"] = verified
+	sess.Save(c.Request(), c.Response())
+
+	return c.Redirect(http.StatusSeeOther, "/")
+}
+
+func requestVerify(c echo.Context) (reqID string, _ error) {
+	return "foobar", nil
+}
+
+func requestCheck(c echo.Context) (verified bool, _ error) {
+	return false, nil
+}
+
+func isValidNumber(tel string) bool {
+	if tel == "" {
+		return false
+	}
+	return true
+}
+
+func isValidPin(pin string) bool {
+	return true
+}
+
+func cleanTel(tel string) string {
+	return tel
 }
 
 func main() {
 	authKey := os.Getenv("SESSION_AUTH_KEY")
 
 	e := echo.New()
+	e.Renderer = t
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(session.Middleware(sessions.NewCookieStore([]byte(authKey))))
 
 	e.GET("/", home)
-	e.GET("/send", send)
-	e.GET("/verify", verify)
+	e.POST("/verify", verify)
+	e.POST("/check", check)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
